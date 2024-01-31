430cfe50379db7e6c1a4aab9b290fb496cf22a4d
Ebaad Zaidi <158239396+Ebaadz@users.noreply.github.com>
2024-01-31 08:48:03 -0500

    Update CardTrick.java
    lucky card update 2

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -10,7 +10,10 @@ package card;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author ebaad
  */
+import java.util.Scanner;
+
 public class CardTrick {
+    
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
@@ -23,10 +26,37 @@ public class CardTrick {
             magicHand[i] = c;
         }
         
-        // Add the luckyCard with card number 2 and suit "Clubs"
-        Card luckyCard = new Card();
-        luckyCard.setValue(2);
-        luckyCard.setSuit("Clubs");
+        // Print the cards in magicHand
+        for (Card card : magicHand) {
+            System.out.println(card.getSuit() + " " + card.getValue());
+        }
+        
+        // Ask the user for input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a card value (1-13): ");
+        int userValue = scanner.nextInt();
+        System.out.print("Enter a suit (0-3 where 0-Hearts, 1-Diamonds, 2-Clubs, 3-Spades): ");
+        int userSuit = scanner.nextInt();
+        scanner.close();
+        
+        // Check if the user's card is in the magicHand
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == userValue && card.getSuitIndex() == userSuit) {
+                found = true;
+                break;
+            }
+        }
+        
+        // Report the result
+        if (found) {
+            System.out.println("Congratulations, your card is in the magic hand!");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
+    }
+}
+
         
         // Insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
